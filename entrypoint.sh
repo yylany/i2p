ed590106d48b28c7269422d2d76d61b0470cb55e
yaoyonglong <1906600192@qq.com>
2020-09-16 12:10:27 +0800

    初步完成

---
+++ entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+chown -R i2p:nobody /opt/i2p
+chmod -R u+rwx /opt/i2p
+
+privoxy /etc/privoxy/config && gosu i2p /opt/i2p/i2psvc /opt/i2p/wrapper.config wrapper.pidfile=/var/tmp/i2p.pid \
+   wrapper.name=i2p \
+   wrapper.displayname="I2P Service" \
+   wrapper.statusfile=/var/tmp/i2p.status \
+   wrapper.java.statusfile=/var/tmp/i2p.java.status \
+   wrapper.logfile=/var/tmp/wrapper.log \
